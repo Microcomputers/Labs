63b260f872502232aa16a73defdcc34dc49d8c14
Amir <amir_rajabifar@yahoo.com.au>
2016-08-26 09:45:18 +0800

    Add source files for lab 3

--- 03-Polling-Keypad-Driver-in-C/Source/main3.c
+++ 03-Polling-Keypad-Driver-in-C/Source/main3.c
@@ -4,10 +4,12 @@ Monitor the keypad and update the LEDs display and the Seven Segment displays
 #include "clic3.h"
 uc_8 temp;
 ui_16 counter;
+int i = 1, j = 0;
 void main(void) {
 // uc_8 temp;
 enum bool flag;
-
+//keypad values
+ui_16 val1, val2, total;
 // System Initialisation
 Initialise();
 // Peripheral Initialisation
@@ -18,9 +20,25 @@ sevenSegInit();
 for(;;) {
 	if(keypadGet(&temp))
 	{
-		flag = LEDsPut(~temp);
-		flag = sevenSegPut(DispID1, temp);
-		flag = sevenSegPut(DispID2, temp);
+		if (i == 1)
+		{
+			val1 = ~temp;
+			flag = LEDsPut(~temp);
+			flag = sevenSegPut(DispID1, val1);	
+			i = 2;		
+		}
+		else if (i == 2)
+		{
+			val2 = ~temp;
+			flag = LEDsPut(~temp);
+			flag = sevenSegPut(DispID1, val2);
+			i = 1;
+		}
+		//total = val1 + val2;
+		//flag = sevenSegPut(DispID1, total);
+		//flag = sevenSegPut(DispID2, total);
+		//i = 1;
+
 	}
 } /* loop forever */
   /* please make sure that you never leave main */
