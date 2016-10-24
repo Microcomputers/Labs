7f3ec81153d00aa1be4e828107f66df8e7e58463
Amir <amir_rajabifar@yahoo.com.au>
2016-10-24 13:01:56 +0800

    I want to die

--- 06-lift-model/Source/main.c
+++ 06-lift-model/Source/main.c
@@ -1,89 +1,119 @@
 //#include <msp430fr5739.h>
 #include "lift1lib.h"
+#include "clic3.h"
 
-/* Time delay function */
-// Software time delay for simplicity here.
-// Assume one cycle of the loop takes approximately 0.1microsecs
-void Delay2(int count);
-//void Delay1(ui_32 DelayConst1);
-void Delay();
+extern ui_16 BusAddress, BusData;
 
-//This two functions test LEDs
-// Turn LEDs ON, on start up
-void turnLEDsON();
-// Turn LEDs OFF, on start up
-void turnLEDsOFF();
-long long int cc;
+
+
+//uc_8 buttonStore[10] = {0,0,0,0,0,0,0,0,0,0};
+uc_8 buttonQueue[20];
 
 int main()
 {
-    uc_8 buttonstat, old_lvl, current_lvl, floorID;
+        ui_32 timer = 0;
+        ui_32 second = 10000000 / 12;
+        ui_32 timeout = 5 * second;
+	uc_8 buttonstat, old_lvl, current_lvl, floorID;
+	int moveUp = 0, moveDown = 0;
 	//1)Initialising the lift system
-    Initialise();
-	liftInit();
 	//2)Moving the lift up, down and stopping it.
-        //lift1Down();
-        //Delay1(3e6);
-        //lift1Stop();
+	//lift1Down();
+	//Delay1(3e6);
+	//lift1Stop();
 	//3)Detecting a button press and returning the button identity to the calling routine
 	//4)Detecting a level in the shaft and returning the level identity to the calling routine
 	//5)Switching on or off a specified LED associated with buttons etc
 	//6)Delaying an approximate time period
 	//7)Opening and closing the lift doors (simulated by time delay operations)
-   
-    turnLEDsON();
-  //  Delay1(2500);
-   turnLEDsOFF();
-    moveInitiPos();	//move lift cage to initial pos
-    //Delay1(2500);
-    cc = 0;
-    while (1)
-    {
-      
-        uc_8 level;
-        int c = 0;
-        
-      
-      floorGet(&floorID);
-      
-        if(buttonGet(&buttonstat))
-        {
-          while(floorID != buttonstat)
-          {
-            if(buttonstat > floorID)
-            {
-              lift1Up();
-              while(floorGet(&floorID)==false){}
-            }
-            
-              if(buttonstat < floorID)
-            {
-              lift1Down();
-              while(floorGet(&floorID)==false){}
-            }
-            
-          }
-          lift1Stop();
-        
-           }
-         
-        else {
-          cc++;
-          if (cc >= 25000000)
-          {
-            cc=0;
-            moveInitiPos();
-          }
-          
-          
-            
-            
-        }
-        
-          
-        
-        }
-      
+	Initialise();
+	liftInit();
+	turnLEDsON();
+	turnLEDsOFF();
+	moveInitiPos();	//move lift cage to initial pos
+	//Determine to move up or down first time round
+	//wait till a button is pressed
+	while(!buttonGet(&buttonstat)){}
+	if (buttonGet(&buttonstat))
+	{
+		processButton(buttonstat);
+		if (buttonstat > level2ID)
+		{
+			moveUp   = 1;
+			moveDown = 0;
+		}
+		if (buttonstat < level2ID)
+		{
+			moveUp   = 0;
+			moveDown = 1;
+		}	
+	}
+	while (1)
+	{
+		timer++;
+		//uc_8 level;
+		//int c = 0;
+		//floorGet(&floorID);
+
+		if(buttonGet(&buttonstat))
+		{
+			timer = 0; //reset timer counter
+			processButton(buttonstat);
+		/*
+		buttonStore[0] = buttonstat;
+		WriteLed1 (buttonStore[0], LEDon); 
+		while(floorID != buttonStore[0])
+		{
+		if(buttonStore[0] > floorID)
+		{
+		lift1Up();
+		while(floorGet(&floorID)==false){}
+		}
+
+		if(buttonStore[0] < floorID)
+		{
+		lift1Down();
+		while(floorGet(&floorID)==false){}
+		}
+		}
+		lift1Stop();
+		WriteLed1 (buttonStore[0], LEDoff); 
+		*/
+		}
+		else 
+		{
+			if (timer >= timeout)
+			{
+				moveInitiPos();
+				timer = 0;
+			}
+		}
+		if (moveUp)
+		{
+			lift1Up();
+			while(floorGet(&floorID)==false){}
+			liftbreak(floorID);
+		}
+		else if (moveDown)
+		{
+			lift1Down();
+			while(floorGet(&floorID)==false){}
+			liftbreak(floorID);    
+		}
+                BusAddress = Lift1Address;
+                BusRead();
+                floorID = (((BusData >> 1) & floorMask & 0x06));
+		if (floorID == bottom)
+		{
+			moveUp   = 1;
+			moveDown = 0;
+		}
+		else if (floorID == top)
+		{
+			moveUp   = 0;
+			moveDown = 1;
+		}
+	}
 }
     
       
@@ -133,39 +163,3 @@ int main()
 	for (i1=0;i1<DelayConst1;i1++){}
 }
 */
-//100 ms delay
-void  Delay()
-{
-	ui_32 DelayConst1 = 1000000;
-	ui_32 i1;
-	for (i1=0;i1<DelayConst1;i1++){}
-}
-
-
-void Delay2(int count)
-{
-	for (int j = 0; j < count; ++j)
-	{
-		Delay();
-	}
-}
-
-//int LEDAddress[] = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd};
-
-void turnLEDsON()
-{
-	for (int i = 0; i < 12; ++i)
-	{
-		WriteLed1(i, LEDon);
-                Delay2(2);
-	}
-}
-
-void turnLEDsOFF()
-{
-	for (int i = 12; i >0; i--)
-	{
-		WriteLed1(i, LEDoff);
-                Delay2(2);
-	}
-}
